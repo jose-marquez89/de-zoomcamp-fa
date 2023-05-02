4bd5df62198d5f54cdc0a811df440b415f22ee4b
Jose Marquez <jlmarquez89@gmail.com>
2023-05-02 14:00:09 -0500

    adds code to ingest parquet

--- wk1/docker-sql/parquet_processing.py
+++ wk1/docker-sql/parquet_processing.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import argparse
 import os
 import pandas as pd
 
 from sqlalchemy import create_engine
 
-df = pd.read_parquet('data/yellow_tripdata_2021-01.parquet')
-df.head()
+def load_data(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name 
+    url = params.url
+    parquet_fname = "data/pq_out.parquet"
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    os.system(f"wget {url} -O {parquet_fname}")
 
-print(pd.io.sql.get_schema(df, name="yellow_taxi_data", con=engine))
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-df.head(n=0).to_sql(name="yellow_taxi_data", con=engine, if_exists='replace')
+    df = pd.read_parquet(parquet_fname)
+    pd.io.sql.get_schema(df, name=table_name, con=engine)
 
-df.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+
+    df.to_sql(name=table_name, con=engine, if_exists="append")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ingest parquet data into postgres db")
+    
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres user')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port on which postgres is running')
+    parser.add_argument('--db', help='target database name')
+    parser.add_argument('--table_name', help='database table name')
+    parser.add_argument('--url', help='url of the file')
+
+    args = parser.parse_args()
+
+    load_data(args)
+    
