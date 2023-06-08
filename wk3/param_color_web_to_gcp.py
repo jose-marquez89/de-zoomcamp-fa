4dd5721318e810ecf5d3a021bcc6a42b69d84c1c
Jose Marquez <jlmarquez89@gmail.com>
2023-06-08 21:28:13 +0200

    adds simple single script version to quickly load data

---
+++ wk3/param_color_web_to_gcp.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+from time import time
+from datetime import timedelta
+
+import pandas as pd
+from prefect import task, flow
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect.tasks import task_input_hash
+
+# remember that the cache key fn keeps you from having to re-read
+# the dataset more than once if it has already been read
+@task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read data into a pandas df"""
+    print(f"Reading file: \n\t{dataset_url}")
+    df = pd.read_csv(dataset_url)
+
+    return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
+    """Fix dtypes in df"""
+    if color == "yellow":
+        dt_letter = "t"
+    else:
+        dt_letter = "l"
+        df['trip_type'] = df['trip_type'].fillna(-99).astype("int")
+    
+    pu_col = f"{dt_letter}pep_pickup_datetime"
+    do_col = f"{dt_letter}pep_dropoff_datetime"
+
+    df['VendorID'] = df['VendorID'].fillna(-99).astype("int")
+    df['RatecodeID'] = df['RatecodeID'].fillna(-99).astype("int")
+    df['payment_type'] = df['payment_type'].fillna(-99).astype("int")
+    df['PULocationID'] = df['PULocationID'].astype("float")
+    df['DOLocationID'] = df['DOLocationID'].astype("float")
+    df['passenger_count'] = df['passenger_count'].fillna(0).astype('int')
+    df[pu_col] = pd.to_datetime(df[pu_col])
+    df[do_col] = pd.to_datetime(df[do_col])
+
+    print(df.head(2))
+    print(f"Column Types: {df.dtypes}")
+    print(f"Total Rows: {df.shape[0]}")
+    return df
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload the local parquet file to GCS"""
+    gcs_bucket = GcsBucket.load("dez-prefect-gcs")
+    gcs_bucket.upload_from_path(from_path=f"{path}", to_path=path)
+
+@task()
+def write_local(df: pd.DataFrame, dataset_file: str, color: str) -> Path:
+    """Write dataframe as parquet"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+
+    return path
+
+@flow()
+def etl_web_to_gcs(year: int, month: int, color: str) -> None:
+    """Main ETL function"""
+    # TODO: find out what the colon in the last f string parameter does
+    # - I've never seen this python feature before
+    dataset_file = f"{color}_tripdata_{year}-{month:02}" 
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz" 
+
+    df = fetch(dataset_url)
+    cleaned  = clean(df, color)
+    path = write_local(cleaned, dataset_file, color)
+    write_gcs(path)
+
+@flow()
+def etl_parent_flow(months: list[int] = [1, 2], 
+                    year: int = 2021,
+                    color: str = "yellow") -> None:
+    
+    for month in months:
+        print(f"Running month {month}...")
+        etl_web_to_gcs(year, month, color)
+
+if __name__ == "__main__":
+    color = "green"
+    year = 2020 
+    months = list(range(1, 13)) 
+    etl_parent_flow(months, year, color)
